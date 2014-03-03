8b8af15436932815945f0b93492b42d81293dbe1
zarawu <zarachongzi@gmail.com>
2014-03-03 16:23:26 +0800

    project committed
    all vesion 0.1 committed

---
+++ Assets/ZombieAnimator.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+public var sprites:Sprite[];
+public var framesPerSecond:float;
+private var spriteRenderer:SpriteRenderer;
+function Start () {
+//初始化变量
+	spriteRenderer = renderer as SpriteRenderer;
+	
+
+}
+
+function Update () {
+	/*
+	关卡载入到当前的时间秒数和每秒渲染的帧数相乘.
+	如果动画帧是存储在一个无限长的数组里,得出的数就是数组中成员的索引数.
+	*/
+	var index:int=Time.timeSinceLevelLoad*framesPerSecond;
+	index =index%sprites.Length;//通过取余实现循环
+	spriteRenderer.sprite=sprites[index];
+	
+}
---
+++ Assets/ZombieControl.js
@@ -0,0 +1,60 @@
+﻿#pragma strict
+public var moveSpeed:float;//moveSpeed将存储一个units数,这个单位不是像素,而是僵尸每秒移动的速度.
+						//因为现在精灵是一个单位为100个像素,所以这个值可能需要相当小.
+private var moveDirection:Vector3;
+
+public var turnSpeed:float;//控制僵尸定位自己方向的响应速度
+function Start () {
+	moveDirection = Vector3.right;
+}
+
+function Update () {
+//有输入时间时更新moveDirection
+	var currentPosition:Vector3=transform.position;
+	print("currentPosition="+currentPosition);
+	if(Input.GetButton("Fire1")){//FIre1:鼠标左键
+	print("Fire1 on click");
+		var moveToward:Vector3=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		print("Fire1 moveToward="+moveToward);
+		moveDirection=moveToward-currentPosition;
+		print("Fire1 moveDirection="+moveDirection);
+		moveDirection.z=0;
+		moveDirection.Normalize();
+	}
+	
+//僵尸跟随鼠标走路的效果:
+	var target:Vector3=moveDirection*moveSpeed+currentPosition;
+	transform.position=Vector3.Lerp(currentPosition,target,Time.deltaTime);
+	
+	var targetAngle=Mathf.Atan2(moveDirection.y,moveDirection.x)*Mathf.Rad2Deg;
+	transform.rotation=Quaternion.Slerp(transform.rotation,
+	Quaternion.Euler(0,0,targetAngle),
+	turnSpeed*Time.deltaTime);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
